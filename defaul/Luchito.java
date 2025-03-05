8132a6477a7b25ed396f8a79147174fbd5fee13e
Sebastian <sebastianbn465@gmail.com>
2025-03-04 20:53:10 -0500

    Archivo 1 - Trabajillo de ATS

--- defaul/Luchito.java
+++ defaul/Luchito.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Luchito {
 
 	public static void main(String[] args) {
+		double Pers1, Pers2, Pers3, resul;
+		Scanner sc = new Scanner( System.in);
+		System.out.print("Ingrese el salario de Guillermo: " );
+		Pers1 = sc.nextDouble();
+		Pers2 = Pers1/2;
+		Pers3 = (Pers1 + Pers2)/2;
+		resul = Pers1 + Pers2 + Pers3;
+		System.out.println("La suma es: "+resul);
 		
-		Scanner Pepe = new Scanner(System.in);
-		System.out.println("Ingresa un número");
-		int numero = Pepe.nextInt();
-		System.out.println("Tu número es: " + numero);
-		// TODO Auto-generated method stub
+		
+	    
+		
 
 	}
  
